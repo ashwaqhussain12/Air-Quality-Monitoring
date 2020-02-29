1fe291ba187abcb6d18d746ef7b85e910a8e2399
Ashwaq Hussain <50024113+ashwaqhussain12@users.noreply.github.com>
2020-02-29 18:15:54 +0530

    Update csvone.py

--- csvone.py
+++ csvone.py
@@ -11,7 +11,8 @@ rows = []
 # reading csv file 
 with open(filename, 'r') as csvfile: 
 	# creating a csv reader object 
-	csvreader = csv.reader(csvfile) 
+	csvreader = csv.reader(csvfile)
+	#print(type(csvreader))
 	
 	# extracting field names through first row 
 	fields = next(csvreader) 
@@ -36,9 +37,29 @@ for row in rows[:3]:
 		print("%10s"%col), 
 	print('\n') 
 print(type(rows))
-"""
 
 #printing tuple by tuple
 for rowln in rows:
 	print(" ".join(map(str, rowln)))
+"""                                 
 
+# Deleting tuples with NA Values
+sublst = [subl for subl in rows if subl[5] != 'NA']
+# print(type(sublst))
+
+"""
+# printing first 3 rows after deleting Null Values
+print('\nfirst 3 rows are:\n') 
+for row in sublst[:3]: 
+	# parsing each column of a row 
+	for col in row: 
+		print("%10s"%col), 
+	print('\n')
+"""
+#printing tuple by tuple
+for subln in sublst:
+	print(" ".join(map(str, subln)))
+
+# Printing the length of list after Deleting NA values
+totdatar=len(sublst)
+print("Total no. of Rows After Deleting NA values: %d"%(totdatar))
