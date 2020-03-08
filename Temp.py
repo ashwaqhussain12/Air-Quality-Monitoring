497a9788b295039c7246387a429308f3711aa248
Ashwaq Hussain <50024113+ashwaqhussain12@users.noreply.github.com>
2020-03-08 16:13:37 +0530

    Update Temp.py

--- Temp.py
+++ Temp.py
@@ -1,13 +1,5 @@
-data3 = pd.read_csv('PRSA_data.csv')
-#data3 = data3.PM_2point5.fillna(72, inplace=True)
-#print(data3.head())
-data3['datep'] = data3[data3.columns[1:4]].apply(lambda x: '-'.join(x.astype(str)),axis=1)
-#data3['date'] = pd.to_datetime(data['date'], infer_datetime_format=True)
-#print(data3.head())
-data5 = pd.read_csv('PRSA_data.csv', parse_dates = [['year', 'month', 'day', 'hour']])
-#data5[year_month_day_hour] = pd.datetime(data5[year_month_day_hour], format = '%d/%m/%y %h')
-#print(data5.head())
-#print(type(data5))
+
+import pandas as pd
 import numpy as np
 import plotly
 import matplotlib.pyplot as plt
@@ -17,6 +9,13 @@ import warnings
 warnings.filterwarnings("ignore")
 
 from fbprophet import Prophet
+
+data3 = pd.read_csv('PRSA_data.csv')
+#data3 = data3.PM_2point5.fillna(72, inplace=True)
+#print(data3.head())
+data3['datep'] = data3[data3.columns[1:4]].apply(lambda x: '-'.join(x.astype(str)),axis=1)
+
+data5 = pd.read_csv('PRSA_data.csv', parse_dates = [['year', 'month', 'day', 'hour']])
 
 #templst = list(data3['PRES'])
 #print(templst)
@@ -30,7 +29,7 @@ print(temp.head())
 
 # initializing the fbprophet model and fitting the data
 model = Prophet()
-model.add_regressor(data3['PRES'])
+model.add_regressor(data3['PRES']) #her i am getting an error ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
 #model.add_regressor('DEWP')
 #model.add_regressor('TEMP')
 #model.add_regressor('IWS')
